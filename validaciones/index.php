113bce3d7d0fe7b1dbb4b1112aad8be7d1d56715
BrandonStambuk <115127679+BrandonStambuk@users.noreply.github.com>
2022-10-22 21:01:54 -0400

    Add files via upload
    validaciones de los formularios formando parte de las tareas propuestas en el taiga

---
+++ validaciones/index.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'includes/conecta.php';
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to you WebApp</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <?php require 'partials/header.php' ?>
+
+    <?php if(!empty($user)): ?>
+      <br> Welcome. <?= $user['email']; ?>
+      <br>You are Successfully Logged In
+      <a href="logout.php">
+        Logout
+      </a>
+    <?php else: ?>
+      <h1>Please Login or SignUp</h1>
+
+      <a href="login.php">Login</a> or
+      <a href="signup.php">SignUp</a>
+    <?php endif; ?>
+  </body>
+</html>
+
+
+
