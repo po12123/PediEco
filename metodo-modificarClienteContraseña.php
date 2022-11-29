2e3bdb23bd70f01d9d293c23e2c8c95a78abaf3d
henrryllanos <113366048+henrryllanos@users.noreply.github.com>
2022-11-29 22:43:23 +0100

    detalle de produto subido

--- metodo-modificarClienteContraseña.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'includes/conecta.php';
-$idCliente = $conecta->real_escape_string($_POST['id']);
-$nuevaContrasenia = $conecta->real_escape_string(md5($_POST['con']));
-
-$actualizar = "UPDATE clientes SET Password_Cliente = '$nuevaContrasenia' WHERE Id_Clientes = $idCliente";
-$conecta -> query($actualizar);
-?>
--- metodo-modificarEncargadoContraseña.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'includes/conecta.php';
-$idEncargado = $conecta->real_escape_string(($_POST['id']));
-$nuevaContrasenia = $conecta->real_escape_string(md5($_POST['con']));
-
-$actualizar = "UPDATE establecimiento SET Password_Encargado = '$nuevaContrasenia' WHERE Id_Establecimiento = $idENcargado";
-$conecta -> query($actualizar);
-?>
